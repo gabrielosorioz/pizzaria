7eb32cbf3b1ee4f8a8d01135731f9831825c388e
gabrielosorioz <gabrielosorio013@gmail.com>
2023-10-13 23:02:26 -0300

    fix: classe cliente

--- src/main/java/com/universidade/pizzaria/service/ClienteService.java
+++ src/main/java/com/universidade/pizzaria/service/ClienteService.java
@@ -1,10 +1,8 @@
 package com.universidade.pizzaria.service;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.universidade.pizzaria.entity.Cliente;
 import com.universidade.pizzaria.repository.ClienteRepository;
 
@@ -34,5 +32,6 @@ public class ClienteService {
     public void excluir(Long id){
         Cliente deletedCliente = clienteRepository.findById(id).get();
         clienteRepository.delete(deletedCliente);   
+
     }
 }
