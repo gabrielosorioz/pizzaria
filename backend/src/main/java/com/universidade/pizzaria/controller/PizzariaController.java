0c57b907df7e5e378f0a6903847f9b3f047d25ae
gabrielosorioz <gabrielosorio013@gmail.com>
2023-10-23 19:00:42 -0300

    feat: correcao

--- backend/src/main/java/com/universidade/pizzaria/controller/PizzariaController.java
+++ backend/src/main/java/com/universidade/pizzaria/controller/PizzariaController.java
@@ -1,18 +1,10 @@
 package com.universidade.pizzaria.controller;
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import com.universidade.pizzaria.entity.Produto;
-import com.universidade.pizzaria.service.ProdutoService;
-
 @Controller
 public class PizzariaController {
     
-    @Autowired
-    private ProdutoService produtoService;
-
+    
     @GetMapping("/home")
     public String pizzaPage() {
         return "index";
